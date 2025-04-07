9853ff6dde7017a0a9f80419767c3f4f6fa50a89
zsar.daniel <zsar.daniel@C19-TANAR>
2025-04-07 08:15:59 +0200

    Add project files.

---
+++ 11_x_WPF_file_interaktiv/FileManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace _11_x_WPF_file_interaktiv
+{
+    class FileManager
+    {
+        private string fileName;
+        public FileManager(string fileName) {
+            this.fileName = fileName;
+        }
+        public List<Person> ReadFile() {
+            List<Person> all = new List<Person>();
+            try
+            {
+                StreamReader read = new StreamReader(fileName, Encoding.UTF8);
+                read.ReadLine();
+                while (!read.EndOfStream)
+                {
+                    all.Add(new Person(read.ReadLine()));
+                }
+                read.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return all;
+        }
+        public void WriteFile(List<Person> all) {
+            try
+            {
+                StreamWriter write = new StreamWriter(fileName, false, Encoding.UTF8);
+                write.WriteLine("Első sor");
+                foreach (Person item in all)
+                {
+                    write.WriteLine(item.Height + "-" + item.Weight);
+                }
+                write.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
---
+++ 11_x_WPF_file_interaktiv/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_x_WPF_file_interaktiv
+{
+    class Person
+    {
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public Person(string sor) {
+            string[] temp = sor.Split('-');
+            Height = int.Parse(temp[0]);
+            Weight = int.Parse(temp[1]);
+        }
+        public Person(int Height, int Weight) {
+            this.Height = Height;
+            this.Weight = Weight;
+        }
+        public Person() {
+            Weight = 80;
+            Height = 190;
+        }
+    }
+}
